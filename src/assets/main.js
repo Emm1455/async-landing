3eda97145f04bc2b193bf5047eee7ebca4018bb9
Emmanuel Mercado <emmanuelf21004@gmail.com>
2022-12-09 09:06:27 -0400

    Cleaning the code

--- src/assets/main.js
+++ src/assets/main.js
@@ -19,27 +19,6 @@ async function fetchData(urlApi){
     try{
         const videos = await fetchData(API);
 
-        // let view = '';
-        // for (let i = 0; i<4 ;i++){
-        //     let imgUrl = videos.contents[i].video.thumbnails[3].url;
-        //     let title = videos.contents[i].video.title;
-        //     view = view + `
-        //     <a href="https://youtube.com/watch?v=${videos.contents[i].video.videoId}" target="_blank">
-        //     <div class="group relative">
-        //         <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-        //             <img src="${imgUrl}" alt="lala" class="w-full">
-        //         </div>
-        //         <div class="mt-4 flex justify-between">
-        //             <h3 class="text-sm text-gray-700">
-        //                 <span aria-hidden="true" class="absolute inset-0"></span>
-        //                 ${title}
-        //             </h3>
-        //         </div>
-        //     </div>
-        //     </a>`;
-        // }
-
-        // let imgUrl = videos.contents[0].video.thumbnails[3].url;
         let view = `${videos.contents.map(video => `
         <div class="group relative">
         <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
